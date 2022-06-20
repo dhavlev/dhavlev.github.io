2b88848de5b063526c6a212390b870dea217b679
visharad dhavle <visharad.dhavle@gmail.com>
2022-06-20 15:50:49 +0000

    readme updated

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# This is my website
+### Preparing machine ubuntu 20.04
 
-## This is what I do
-1. A
-2. B
-3. C
-4. D
-5. E
-6. F
+#### Install Ruby and other prerequisites:
+```sh
+sudo apt get update
+sudo apt get install
+sudo apt-get install ruby-full build-essential zlib1g-dev
+```
+
+Avoid installing RubyGems packages (called gems) as the root user. Instead, set up a gem installation directory for your user account. The following commands will add environment variables to your ~/.bashrc file to configure the gem installation path:
+```sh
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+```
+
+Finally, install Jekyll and Bundler:
+```sh
+gem install jekyll bundler
+```
+That’s it! You’re ready to start using Jekyll.
+
+#### At the source
+```sh
+bundle install
+```
+#### To serve the pages
+
+```sh
+bundle exec jekyll serve
+```
+
+
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Foo Bar Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/items/1",
+        "teaser": "/assets/images/foo-bar-identity-th.jpg"
+      },{
+        "title": "Foo Bar Identity",
+        "excerpt":"COPY Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War...","categories": [],
+        "tags": [],
+        "url": "/portfolio/items/2",
+        "teaser": "/assets/images/foo-bar-identity-th.jpg"
+      },{
+        "title": "Test Posts",
+        "excerpt":"this is my test post  ","categories": [],
+        "tags": [],
+        "url": "/test-posts/",
+        "teaser": null
+      },{
+        "title": "Test Posts2",
+        "excerpt":"this is my test post 2  ","categories": [],
+        "tags": [],
+        "url": "/test-posts2/",
+        "teaser": null
+      },{
+        "title": "Three years of problems with an iMac G5 02",
+        "excerpt":"TECH POST 02  ","categories": ["articles"],
+        "tags": ["Apple","tech geek"],
+        "url": "/articles/tech-posts-copy/",
+        "teaser": null
+      },{
+        "title": "Three years of problems with an iMac G5",
+        "excerpt":"TECH POST 01  ","categories": ["articles"],
+        "tags": ["Apple","tech geek"],
+        "url": "/articles/tech-posts/",
+        "teaser": null
+      }]
